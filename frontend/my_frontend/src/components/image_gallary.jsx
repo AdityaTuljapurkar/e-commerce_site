a247c13fb0a76d9e05036dc849803a6d3d76c1a6
Aditya Tuljapurkar <adityatuljapurkar90018@gmail.com>
2025-04-15 01:58:28 +0530

    v 1.0.2 added css

--- frontend/my_frontend/src/components/image_gallary.jsx
+++ frontend/my_frontend/src/components/image_gallary.jsx
@@ -7,22 +7,24 @@ import img2 from '../images/img2.avif';
 import img3 from '../images/img3.avif';
 import img4 from '../images/img4.avif';
 import img5 from '../images/img5.avif';
-
+import '../css/image_grallary.css';
 const ImageGallary = () => {
 const images = [img1, img2, img3, img4, img5];
 const [index, setIndex] = useState(0);
-
-useEffect(() => {
-    const interval = setInterval(() => {
+useEffect(()=>{
+    const interval = setInterval(()=>{
         setIndex(index => (index + 1) % images.length);
-    }, 6000);
-    return () => clearInterval(interval);
-}, [images.length]);
-
-return (
-    <div id="image_grallery">
-        <img src={images[index]} alt={`Image -> ${index}`} />
+    }
+    , 4000);
+    return()=> clearInterval(interval)
+    
+},[])
+return(
+    <div>
+        <h1>Image Grallary</h1>
+        <div id ='image-gallary'>
+            <img src={images[index]} alt={`Image -> ${index}`}  />
+        </div>
     </div>
-);
-}
+);}
 export default ImageGallary;
